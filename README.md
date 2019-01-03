c9d54f50dd38709553d005ec95b2d9c6df88733c
Cyrus Tabatabai-Yazdi <ctabatab@gmail.com>
2019-01-02 21:04:55 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web-Blog(In Progress)
+
+A web blog written using Flask, Python, and MongoDB
+Allows for users to register, create blogs, make posts, and view the blogs of other users
+
---
+++ src/app.py
@@ -0,0 +1,111 @@
+from flask import Flask,render_template,request,session,make_response
+from user import User
+from database import Database
+from blog import Blog
+from post import Post
+
+
+app = Flask(__name__) # '__main__'
+app.secret_key ="rolandGarros"
+
+
+@app.route('/')
+def home_template():
+    return render_template("home.html")
+
+@app.route('/login') #www.mysite.com/api
+def login_template():
+    return render_template('login.html')
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
+
+@app.before_first_request #run before first request
+def initialize_database():
+    Database.initialize()
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+
+    if User.login_valid(email,password):
+        User.login(email)
+    else:
+        session['email'] = None
+        return render_template('home.html')
+
+
+    return render_template('profile.html',email=session['email'])
+
+@app.route('/auth/register',methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+
+    User.register(email,password)
+
+    return render_template('profile.html',email=session['email'])
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_by_id(user_id)
+    else:
+        user = User.get_by_email(session['email'])
+    
+    if user is not None:
+        blogs = user.get_blogs()
+
+        return render_template("user_blogs.html",blogs=blogs,email=user.email)
+    
+    return render_template("home.html")
+
+@app.route('/blogs/new',methods=['POST','GET'])
+def create_new_blog():
+    if request.method == 'GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email,title,description,user._id)
+        new_blog.save_to_mongo()
+        return make_response(user_blogs(user._id))
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template('posts.html',posts=posts,blog_title=blog.title,blog_id=blog._id,author=blog.author,session_id=session['email'])
+
+
+@app.route('/all_blogs')
+def get_all_blogs():
+    blogs = Blog.find_all_blogs()
+
+    return render_template("all_blogs.html",blogs=blogs)
+
+@app.route('/posts/new/<string:blog_id>',methods=['POST','GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_posts.html',blog_id=blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id,title,content,user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
+if __name__ == "__main__":
+    app.run()
+
+
